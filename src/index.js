b3b5034e96cf0394de1d46f81506b07460850f0e
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-16 09:59:10 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+/*-----------------------------------------------------------------
+Challenge: 13-mumble
+Difficulty: Intermediate
+Prompt:
+- Write a function called mumble that accepts a single string argument.
+- The function should return a string that has each character repeated the number of times according to its position within the string arg.  In addition, each repeated section of characters should be separated by a hyphen (-).
+- Examples describe it best..
+Examples:
+mumble('X'); //=> 'X'
+mumble('abc'); //=> 'a-bb-ccc'
+mumble('121'); //=> '1-22-111'
+mumble('!A 2'); //=> '!-AA-   -2222'
+-----------------------------------------------------------------*/
+// Your solution for 13-mumble here:
+
+</div>
+`;
